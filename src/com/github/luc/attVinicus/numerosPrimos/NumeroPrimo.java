75a6a078414fcc9ff27350b08b63371a887630dd
Lucas Silva <lucascamposdrr2022@gmail.com>
2025-04-10 14:10:58 -0300

    Commit inicial

---
+++ src/com/github/luc/attVinicus/numerosPrimos/NumeroPrimo.java
@@ -0,0 +1,30 @@
+package com.github.luc.attVinicus.numerosPrimos;
+
+import java.util.ArrayList;
+
+public class NumeroPrimo {
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        for (int i = 1; i < 25; i++){ // MODIFICAR O I < (NUMERO) PARA MEXER NA QTD DE NUMEROS
+            numeros.add(i);
+        }
+        verficarNumeroPrimo(numeros);
+    }
+
+    public static void verficarNumeroPrimo(ArrayList<Integer> lista) {
+        for (Integer integer : lista) {
+            boolean isPrimo = true;
+            int numAnalise = integer;
+            if (numAnalise <= 1) {
+                isPrimo = false;
+            }
+            for (int j = 2; j < numAnalise - 1; j++) {
+                if (numAnalise % j == 0) {
+                    isPrimo = false;
+                    break;
+                }
+            }
+            System.out.println(numAnalise + " " + isPrimo);
+        }
+    }
+}
