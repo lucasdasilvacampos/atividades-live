a126a8695f6cf5662849a2c962024b5161344cb5
Lucas Silva <lucascamposdrr2022@gmail.com>
2025-04-10 14:47:40 -0300

    feat: implementação do fibonacci

---
+++ src/com/github/luc/codigosAvussos/Fibonacci.java
@@ -0,0 +1,17 @@
+package com.github.luc.codigosAvussos;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10;
+        int f1 = 0;
+        int f2 = 1;
+
+        System.out.print(f1 + " " + f2);
+        for (int i = 2; i < n; i++) {
+            int f3 = f1 + f2;
+            System.out.print(" " + f3);
+            f1 = f2;
+            f2 = f3;
+        }
+    }
+}
